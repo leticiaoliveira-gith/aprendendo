2ae08bcdcb37f9b5880505f2a6bd9ea479ebbcec
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-03-12 13:25:52 -0300

    if e else exercicio

---
+++ ex03.js
@@ -0,0 +1,8 @@
+var idade = 87
+if (idade<16){
+    console.log(`vc não pode votar`)
+} else if (idade <18 || idade>65){
+    console.log(`voto opcional`)
+} else  {
+    console.log(`voto obrigatorio`)
+}
