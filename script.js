8b3098af0ddb899b612bc87f356115e9fbb97669
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-03-14 15:42:21 -0300

    animações

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ function carregar() {
     var img = window.document.getElementById(`imagem`)
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 13
+    //var hora = 18
     msg.innerHTML =`Agora são ${hora} horas`
+
     if (hora >= 0 && hora < 12){//bom dia
+        msg2.innerHTML = `Bom Dia`
         img.src =`banco de imagens/manhã.jpg`
         document.body.style.background = `linear-gradient(to bottom, #f83c03,  #f1a65f)`
+
     } else if (hora >=12 && hora <18){//boa tarde
+        msg2.innerHTML = `Boa Tarde`
         img.src = `banco de imagens/tarde.jpg`
         document.body.style.background = `linear-gradient(to bottom,  #f1a65f, #e958bd)`
+
     } else{//boa noite
+        msg2.innerHTML = `Boa Noite`
         img.src = `banco de imagens/noite.jpg` 
         document.body.style.background =`linear-gradient(to bottom, #0c0427, #71427c)`  
     }
