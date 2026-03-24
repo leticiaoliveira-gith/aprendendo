ca4809ffe089ee445bfb21845ed8d3ff5b2672fc
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-03-24 14:44:15 -0300

    estilização e animação

--- tabuada.js
+++ tabuada.js
@@ -2,19 +2,32 @@ function tabuada(){
     let num = document.getElementById (`num`)
     let tab = document.getElementById (`seltab`)
 
-    if (num.value.length==0){
+    if (num.value.length == 0){
         window.alert(`digite um número`)
     } else {
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ``
-        while (c<= 10){
+        /*while (c<= 10){
            let item = document.createElement(`option`) 
            item.text = `${n} x ${c} = ${n*c}`
            item.value =`tab${c}`
            tab.appendChild(item)
            c++
+        } */
+       function adicionarItem(){
+        if (c <= 10){
+            let item = document.createElement(`option`)
+            item.text =`${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+
+            item.style.animationDelay =`${c*0.05}s`;
+            tab.appendChild(item)
+            c++
+            setTimeout(adicionarItem,100)
         }
+       }
+       adicionarItem()
     }
     
 
