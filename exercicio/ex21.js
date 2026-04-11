6da75a6578617e0f5c52efb23686f57b52f53a28
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-04-10 23:10:12 -0300

    corrigindo nome

--- exercicio/ex21.js
+++ exercicio/ex21.js
@@ -1,18 +1,18 @@
 function checarPerigo(ladoA, humanoPresente) {
     let lobo = ladoA.includes("lobo");
     let ovelha = ladoA.includes("ovelha");
-    let alfafa = ladoA.includes("alfafa");
+    let alface = ladoA.includes("alface");
 
     if (!humanoPresente) {
         if (lobo && ovelha) {
             return "ERRO: O lobo jantou a ovelha!";
         }
-        if (ovelha && alfafa) {
-            return "ERRO: A ovelha jantou a alfafa!";
+        if (ovelha && alface) {
+            return "ERRO: A ovelha jantou a alface!";
         }
     }
     return "Tudo seguro por enquanto...";
 }
 
 // Teste: Lobo e Ovelha sozinhos no Lado A
-console.log(checarPerigo(["ovelha", "alfafa"], false));
+console.log(checarPerigo(["ovelha", "alface"], false));
