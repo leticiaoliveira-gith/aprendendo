1bc0bbf708cef9af1016558ff005b224cd8fa234
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-04-11 22:46:49 -0300

    salario por hora

---
+++ exercicio/ex23.js
@@ -0,0 +1,11 @@
+let funcionario = "kleitim"
+console.log(`o funcionario ${funcionario} recebe:`)
+let salarioM = 25 00
+let horasM = 220
+
+let salarioH = salarioM / horasM
+let resultado= salarioH.toFixed(2).replace(".", ",")
+
+console.log("R$ " + resultado)
+
+
