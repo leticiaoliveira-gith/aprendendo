77fd9bc18771d2376b6ce538bf08fa0413e297c6
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-04-11 22:49:27 -0300

    implementar por hora

--- exercicio/ex23.js
+++ exercicio/ex23.js
@@ -1,11 +1,11 @@
 let funcionario = "kleitim"
 console.log(`o funcionario ${funcionario} recebe:`)
-let salarioM = 25 00
+let salarioM = 2500
 let horasM = 220
 
 let salarioH = salarioM / horasM
 let resultado= salarioH.toFixed(2).replace(".", ",")
 
-console.log("R$ " + resultado)
+console.log("R$ " + resultado + " por hora")
 
 
