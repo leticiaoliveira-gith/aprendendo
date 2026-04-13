79cd4bf7c22991a023427b982d6e6ff3729826c5
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-04-13 16:40:55 -0300

    tipos de triangulo

--- exercicio/ex22.js
+++ exercicio/ex22.js
@@ -1,9 +1,21 @@
 let l1 = 4
 let l2 = 4
-let l3 = 4
+let l3 = 2
+//-------informações-------------
+let altura = 7
+let base = 10
 
 if (l1 ==l2 && l2==l3){
-    console.log(`e um triangulo equilatero`)
-} else{
-    console.log(`n e equiltero`)
+    console.log(`É um triangulo equilatero`)
+    let areaE = (l1 ** 2 * Math.sqrt(3)) / 4
+    console.log(`E sua Área é; ${areaE.toFixed(2)}`)
+
+} else if(l1!=l2 && l2 != l3 && l1!=l3){
+    console.log(`É e um triangulo escaleno`)
+    let areaE = (base*altura)/ 2
+    console.log(`E sua Área é; ${areaE.toFixed(2)}`)
+} else {
+    console.log(`É um triangulo isósceles `)
+    let areaE = (base*altura)/ 2
+    console.log(`E sua Área é; ${areaE.toFixed(2)}`)
 }
