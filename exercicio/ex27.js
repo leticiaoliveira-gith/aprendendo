c06099c5c521d86496f4e0f2f5cc1df2a80bd787
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-04-14 15:12:35 -0300

    sistema cinema

--- exercicio/ex27.js
+++ exercicio/ex27.js
@@ -17,4 +17,24 @@ function calcularPreco(tipo, estudante){
     }
     return precoBase
 }
-console.log(calcularPreco("vip", true)); 
+const listaConvidados = [
+    { nome: "Ana", idade: 20, tipoIngresso: "vip", estudante: true },
+    { nome: "Livia", idade: 14, tipoIngresso: "comum", estudante: true },
+    { nome: "Marcos", idade: 17, tipoIngresso: "comum", estudante: true }
+];
+function gerarRelatorio(lista) {
+    lista.forEach(pessoa => {
+        // Aqui usamos as funções que você criou antes!
+        let valor = calcularPreco(pessoa.tipoIngresso, pessoa.estudante);
+        let autorizado = podeAssistir(pessoa.idade);
+
+        if (autorizado) {
+            console.log(`${pessoa.nome} pagou R$ ${valor} e entrou no cinema.`);
+        } else {
+            console.log(`${pessoa.nome} tem ${pessoa.idade} anos e não pode entrar.`);
+        }
+    });
+}
+
+// Para executar:
+gerarRelatorio(listaConvidados);
