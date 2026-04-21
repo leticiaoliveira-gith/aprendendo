63867855c7657fc9e7c13b5aa114f5b16dfff984
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-04-20 23:41:29 -0300

    somar array

--- exercicio/ex31.js
+++ exercicio/ex31.js
@@ -1,10 +1,16 @@
 //como eu faco uma lista
 function listaNumero(...numero){
+    //(set)tirar numeros duplicados
     const listalimpa = [...new Set(numero)]
 // serve para ordenar da forma correta, sem isso 10 vai pra frente
     listalimpa.sort((a,b) => a-b)
-    return listalimpa ;
-    
+// somar numeros da lista
+    const somaTotal = listalimpa.reduce((acc,curr)=> acc+curr,0)
+
+    return{
+        lista: listalimpa,//[,]obrigatoria pra total n da erro
+        total: somaTotal
+    }
 }
 
 console.log(listaNumero(7,8,9,8,6,8,8,3,10));
