756fb7cb35e9fff429df67d57ae3fecd1aee0a6a
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-04-20 23:27:01 -0300

    adicionado sort/ordenação

--- exercicio/ex31.js
+++ exercicio/ex31.js
@@ -1,7 +1,10 @@
 //como eu faco uma lista
 function listaNumero(...numero){
     const listalimpa = [...new Set(numero)]
+// serve para ordenar da forma correta, sem isso 10 vai pra frente
+    listalimpa.sort((a,b) => a-b)
     return listalimpa ;
+    
 }
 
-console.log(listaNumero(7,8,9,8,6,8,8,3));
+console.log(listaNumero(7,8,9,8,6,8,8,3,10));
