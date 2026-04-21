0a6f44a048b113b2ddb02e1bcbf04c6da8137c80
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-04-21 20:37:35 -0300

    retorno pra não febre
    adicionar um retorno caso não haja febre

--- exercicio/ex34.js
+++ exercicio/ex34.js
@@ -1,7 +1,11 @@
 function medirTemperatura (...temperatura){
     const febre = temperatura.filter(t=>t>37.5)
     febre.sort((a,b)=> b-a)
+    if (febre.length === 0) {
+
+        return "Tudo normal por aqui!";
+    }
 
     return febre
 }
-console.log (medirTemperatura(36, 38.2, 39, 36.5, 37.4, 40.1))
+console.log (medirTemperatura(36, 36.5, 37.4,))
