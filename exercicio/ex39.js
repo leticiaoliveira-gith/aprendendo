5981dd15f82c3471cdfa7d2cbb9c39301c65ae70
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-04-26 00:00:00 -0300

    adicionar carregador

--- exercicio/ex39.js
+++ exercicio/ex39.js
@@ -14,7 +14,8 @@ const produtos = [
     { nome: "Teclado", preco: 150 },
     { nome: "Mouse", preco: 80 },
     { nome: "Monitor", preco: 900 },
-    { nome: "Fone", preco: 50 }
+    { nome: "Fone", preco: 50 },
+    { nome: "carregador", preco: 50 }
 ];
 
 function listarOfertas(lista) {
