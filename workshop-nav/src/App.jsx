b0a35a9c4b070d0924cc94629c534224a018f6a9
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-05-05 14:06:21 -0300

    Passo 6 concluído

--- workshop-nav/src/App.jsx
+++ workshop-nav/src/App.jsx
@@ -1,13 +1,32 @@
+// Definindo o componente Navbar
 export const Navbar = () => {
   return (
     <nav className="navbar">
       <ul>
         <li className="nav-item">
-          <a href="#">Dashboard</a>
+          <a href='#'>Dashboard</a>
         </li>
-        <li className="nav-item"></li>
-        <li className="nav-item"></li>
+        <li className="nav-item">
+          <a href='#'>Widgets</a> 
+        </li>
+        <li className="nav-item">
+            <a href="#">Configurações</a>
+        </li>
       </ul>
     </nav>
   )
 }
+
+// O componente principal que o main.jsx chama
+function App() {
+  return (
+    <div className="container">
+      <Navbar /> 
+      <main className="main-content">
+        <h1>Meu Dashboard</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
