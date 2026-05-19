379e1f165074c86bfe1095c4f9ceb3c92ea4c5e4
Leticia Oliveira <leticia.oliveira.contato01@gmail.com>
2026-05-18 23:14:41 -0300

    adicionado 2 cards

--- workshop-profile-card/src/App.jsx
+++ workshop-profile-card/src/App.jsx
@@ -12,6 +12,19 @@ export function App() {
   return (
     <div className="flex-container">
       <Card name="Mark" title="Front-End developer" bio="I like to work with different front-end technologies and play video games." />
+
+       <Card 
+        name="Tiffany" 
+        title="Engineering manager" 
+        bio="I have worked in tech for 15 years and love to help people grow in this industry." 
+      />
+      
+      <Card 
+        name="Doug" 
+        title="Back-End developer" 
+        bio="I have been a software developer for over 20 years and I love working with Go and Rust." 
+      />
     </div>
+
   )
 }
